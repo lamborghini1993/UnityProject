1f918f6f47e6b18ce3cbd30af7d162444512c6ce
duoyi-window <1323242382@qq.com>
2018-06-22 16:13:21 +0800

    设置鼠标移动在Cube上变红

--- Tower Defense/Assets/Scripts/MapCube.cs
+++ Tower Defense/Assets/Scripts/MapCube.cs
@@ -1,28 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 // 地图方块管理
 
 public class MapCube : MonoBehaviour {
 
     [HideInInspector] // 不需要显示在面板
-    public GameObject turretGo;
-    public GameObject BuildEffect;
+    public GameObject turretGo; //炮塔
+    public GameObject BuildEffect; //建造时的特效
+    private Color oldColor;
+    private Renderer cubeRenderer;
 
     public void BuildTurret (GameObject turretPerfab) {
         turretGo = GameObject.Instantiate (turretPerfab, transform.position, Quaternion.identity);
-        GameObject effect = GameObject.Instantiate(BuildEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 1);
+        GameObject effect = GameObject.Instantiate (BuildEffect, transform.position, Quaternion.identity);
+        cubeRenderer.material.color = oldColor;
+        Destroy (effect, 1);
     }
 
     // Use this for initialization
     void Start () {
-
+        cubeRenderer = GetComponent<Renderer> ();
+        oldColor = cubeRenderer.material.color;
     }
 
     // Update is called once per frame
     void Update () {
 
     }
+
+    private void OnMouseEnter () {
+        Debug.Log("11");
+        if (turretGo != null || EventSystem.current.IsPointerOverGameObject ()) return; //有炮塔或者点击为ui直接return
+        Debug.Log("----");
+        cubeRenderer.material.color = Color.red;
+    }
+
+    private void OnMouseExit () {
+        cubeRenderer.material.color = oldColor;
+    }
+
 }
